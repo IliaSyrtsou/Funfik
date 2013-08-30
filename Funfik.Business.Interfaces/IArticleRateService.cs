2a4a12cd37ac1c647ff25826760b579f4ab874f0
Ilia Syrtsou <syrtsou.ilia@gmail.com>
2013-08-30 14:58:43 +0200

    Added inheritance from IBusinessService interface to the IArticleRateService interface

--- Funfik.Business.Interfaces/IArticleRateService.cs
+++ Funfik.Business.Interfaces/IArticleRateService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Funfik.Business.Interfaces
 {
-    public interface IArticleRateService
+    public interface IArticleRateService: IBusinessService<Article>
     {
     }
 }
